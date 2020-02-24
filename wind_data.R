3756fda030ac889d98abb87cfa368f071904b112
AmierohAbrahams <3559244@myuwc.ac.za>
2020-02-24 17:03:53 +0200

    update

--- wind_data.R
+++ wind_data.R
@@ -1,1 +1,21 @@
-# Downloaded and extract data i the download python scripts
+# Python script to download wind data
+
+# 10m u-component of wind	
+# m s-1	
+# Eastward component of the 10m wind. It is the horizontal speed of air 
+# moving towards the east, at a height of ten metres above the surface of 
+#the Earth, in metres per second. Care should be taken when comparing this
+# variable with observations, because wind observations vary on small space and time
+#scales and are affected by the local terrain, vegetation and buildings that are represented only on 
+#average in the ECMWF Integrated Forecasting System. This variable can be combined with the V component of 10m
+#wind to give the speed and direction of the horizontal 10m wind.
+
+
+
+# 10m v-component of wind	
+# m s-1	
+# Northward component of the 10m wind. It is the horizontal speed of air moving towards the north, at a height of ten metres above 
+#the surface of the Earth, in metres per second. Care should be taken when comparing this variable with observations, because wind observations
+#vary on small space and time scales and are affected by the local terrain, vegetation and buildings that are represented only on average in the ECMWF 
+#Integrated Forecasting System. This variable can be combined with the U component of 10m wind to give the speed and direction of the horizontal 10m wind.
+
