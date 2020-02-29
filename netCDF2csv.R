f6fc638e45449d50d58f36e787e67f8e02db1203
AmierohAbrahams <3559244@myuwc.ac.za>
2020-02-29 14:34:09 +0200

    update EBUS wind

--- netCDF2csv.R
+++ netCDF2csv.R
@@ -72,7 +72,7 @@ endDate <- str_sub(ncList[length(ncList)], start = 15, end = 22)
 
 
 ################  ERA 5    ##################3
-
+# https://cds.climate.copernicus.eu/api-how-to#use-the-cds-api-client-for-data-access
 
 # Extracting ERA 5 wind u and v variables
 # N/W/S/E coordinate format when downloading
@@ -95,6 +95,7 @@ library(doMC); doMC::registerDoMC(cores = 7)
 # #          1         2         3         4
 # # 12345678901234567890123456789012345678901
 # # wu_daily08-19.nc
+
 
 
 ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily81-99_165.nc'
@@ -122,32 +123,6 @@ u_10_df_BC <- new %>%
 # save(u_10_df_BC , file = "data/u_10_df_BC.RData")
 
 
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily81-99_166.nc'
-nc <- nc_open(ncFile)
-fNameStem <- substr(basename(ncFile), 1, 19)
-v_10.2_df <- ncvar_get(nc, varid = "v10") %>%
-  round(4)
-dimnames(v_10.2_df) <- list(lon = nc$dim$lon$vals,
-                            lat = nc$dim$lat$vals,
-                            time = nc$dim$time$vals)
-nc_close(nc)
-v_10.2_df <- as_tibble(melt(v_10.2, value.name = "v_10"))
-v_10.2_df$time <- as.POSIXct(v_10.2_df$time * 60 * 60, origin = "1900-01-01")
-tidy <- u_10_df %>%
-  separate(col = time, into = c("year", "month","day1"), sep = "-")
-t2 <- tidy %>%
-  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
-  select(day)
-new <- cbind(t2, tidy)
-new <- new %>% 
-  select(lon,lat,year,month ,day,u_10)
-u_10_df <- new %>%
-  unite(year, month, day, col = "date", sep = "-")
-
-
-
-
-
 ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily_165.nc'
 nc <- nc_open(ncFile)
 fNameStem <- substr(basename(ncFile), 1, 14)
@@ -170,10 +145,9 @@ new <- new %>%
 u_10.2df_BC <- new %>%
   unite(year, month, day, col = "date", sep = "-")
 
-
-# save(u_10.2df_BC , file = "data/u_10.2df_BC.RData")
-#######
-
+# BC_wind <- rbind(u_10_df_BC,u_10.2df_BC)
+load("data/BC_wind.RData")
+# THis file too big- Get from 2008-2012
 ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wu_daily08-19.nc'
 nc <- nc_open(ncFile)
 fNameStem <- substr(basename(ncFile), 1, 14)
@@ -197,7 +171,13 @@ u_10.2df <- new %>%
   unite(year, month, day, col = "date", sep = "-")
 
 
-#####
+
+# save(u_10.2df_BC , file = "data/u_10.2df_BC.RData")
+# BC_wind <- rbind(u_10_df_BC,u_10.2df_BC)
+# save(BC_wind , file = "data/BC_wind.RData")
+# 165 - u 
+# 166 - v
+################################################################################################################################
 
 ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wv_daily08-19.nc'
 nc <- nc_open(ncFile)
@@ -222,3 +202,58 @@ u_10.2df <- new %>%
   unite(year, month, day, col = "date", sep = "-")
 
 ERA_5_wind <- rbind(u_10_df, u_10.2_df, u_10.3_df)
+####################################################################################################################################################
+####################################################################################################################################################
+# 166 - V
+
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily81-99_166.nc'
+nc <- nc_open(ncFile)
+fNameStem <- substr(basename(ncFile), 1, 13)
+v_10 <- ncvar_get(nc, varid = "v10") %>%
+  round(4)
+dimnames(v_10) <- list(lon = nc$dim$lon$vals,
+                       lat = nc$dim$lat$vals,
+                       time = nc$dim$time$vals)
+nc_close(nc)
+v_10_df <- as_tibble(melt(v_10, value.name = "v_10"))
+v_10_df$time <- as.POSIXct(v_10_df$time * 60 * 60, origin = "1900-01-01")
+tidy <- v_10_df %>%
+  separate(col = time, into = c("year", "month","day1"), sep = "-")
+t2 <- tidy %>%
+  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
+  select(day)
+new <- cbind(t2, tidy)
+new <- new %>% 
+  select(lon,lat,year,month ,day,v_10)
+v_10_df_BC <- new %>%
+  unite(year, month, day, col = "date", sep = "-")
+
+# save(v_10_df_BC , file = "data/v_10_df_BC.RData")
+
+
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily00-07_166.nc'
+nc <- nc_open(ncFile)
+fNameStem <- substr(basename(ncFile), 1, 14)
+v_10.2 <- ncvar_get(nc, varid = "v10") %>%
+  round(4)
+dimnames(v_10.2) <- list(lon = nc$dim$lon$vals,
+                         lat = nc$dim$lat$vals,
+                         time = nc$dim$time$vals)
+nc_close(nc)
+v_10.2_df <- as_tibble(melt(v_10.2, value.name = "v_10"))
+v_10.2_df$time <- as.POSIXct(v_10.2_df$time * 60 * 60, origin = "1900-01-01")
+tidy <- v_10.2_df %>%
+  separate(col = time, into = c("year", "month","day1"), sep = "-")
+t2 <- tidy %>%
+  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
+  select(day)
+new <- cbind(t2, tidy)
+new <- new %>% 
+  select(lon,lat,year,month ,day,v_10)
+v_10.2df_BC <- new %>%
+  unite(year, month, day, col = "date", sep = "-")
+
+
+# save(v_10.2df_BC , file = "data/v_10.2df_BC.RData")
+# BC_vwind <- rbind(v_10_df_BC,v_10.2df_BC)
+# save(BC_vwind , file = "data/BC_vwind.RData")
--- wind_data.R
+++ wind_data.R
@@ -19,3 +19,40 @@
 #vary on small space and time scales and are affected by the local terrain, vegetation and buildings that are represented only on average in the ECMWF 
 #Integrated Forecasting System. This variable can be combined with the U component of 10m wind to give the speed and direction of the horizontal 10m wind.
 
+#################################################################################################################################################################3
+
+
+# Converting U and V wind variables to wind speed and direction
+
+# load("data/BC_wind.RData")
+# load("data/BC_vwind.RData")
+# 
+# BC_vwind <- BC_vwind %>% 
+#   select(v_10)
+# BC_wind <- cbind(BC_vwind,BC_wind)
+# BC_wind_fin <- BC_wind %>% 
+#   select(lon,lat,date,u_10,v_10)
+
+#save(BC_wind_fin , file = "data/BC_wind_fin.RData")
+
+load("data/BC_wind_fin.RData")
+# Wind speed
+BC_wind_fin$u_squared ='^'(BC_wind_fin$u_10,2)
+BC_wind_fin$v_squared ='^'(BC_wind_fin$v_10,2)
+BC_wind_fin <- BC_wind_fin %>% 
+  mutate(speed = sqrt(u_squared + v_squared))
+# Wind direction 
+
+BC_wind_fin <- BC_wind_fin %>% 
+  mutate(wind_dir_trig_to = atan2(u_10/speed, v_10/speed),
+         wind_dir = wind_dir_trig_to * 180/pi)
+
+BC_wind_complete <- BC_wind_fin %>% 
+  select(lon,lat,date,u_10,v_10,speed,wind_dir)
+
+# save(BC_wind_complete , file = "data/BC_wind_complete.RData")
+
+
+
+
+
