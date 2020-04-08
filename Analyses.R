c751d25c6f6d79c78f9df823f6eb5426afdc2da0
AmierohAbrahams <3559244@myuwc.ac.za>
2020-04-08 18:33:37 +0200

    update

--- Analyses.R
+++ Analyses.R
@@ -95,6 +95,56 @@ summary(anova_CC_dur)
 anova_CalC_dur <- anova_func(df = CalC_metrics)
 summary(anova_CalC_dur)
 
+######################################################################################################################################################33
+
+upwell_season_BC <- upwell_season_BC %>% 
+  mutate(current = "BC")
+
+upwell_season_HC <- upwell_season_HC %>% 
+  mutate(current = "HC")
+
+upwell_season_CC <- upwell_season_CC %>% 
+  mutate(current = "CC")
+
+upwell_season_CalC <- upwell_season_CalC %>% 
+  mutate(current = "CalC")
+
+mertrics_combined <- rbind(upwell_season_BC,upwell_season_HC, upwell_season_CC, upwell_season_CalC)
 
 
+lm_coeff <- function(df){
+  res <- lm(formula = val ~ date_peak, data = df)
+  res_coeff <- as.numeric(res$coefficient[2])
+}
 
+# Calculate all of the linear models
+lm_metrics <- mertrics_combined %>% 
+  ungroup() %>% 
+  dplyr::select(-c(speed:index_end), -c(date:season)) %>% 
+  pivot_longer(cols = c(duration, intensity_mean:rate_decline), 
+               names_to = "var", values_to = "val") %>% 
+  group_by(current, var) %>% 
+  nest() %>% 
+  mutate(slope = purrr::map(data, lm_coeff)) %>% 
+  dplyr::select(-data) %>% 
+  unnest(cols = slope) %>% 
+  # convert from daily to decadal values
+  mutate(slope = round((slope*365.25*10), 2)) %>% 
+  ungroup()
+
+# Cast the results wide for easier use with ANOVA
+lm_metrics_wide <- pivot_wider(lm_metrics, 
+                               id_cols = current, 
+                               names_from = var, values_from = slope)
+
+
+# Here are ANOVAs on the linear model results
+# NB: This is not advisable as these linear models are based on only 4 years of data.
+# That is not enough to draw any conclusions about rates of change over time
+# I suppose the arguments could be made that one is not measuring lon term change, 
+# but rather than the data products should show reasonable agreement for events detected
+# within any gicen year.
+summary(aov(duration ~ site + product + distance, data = lm_metrics_wide))
+summary(aov(intensity_mean ~ site + product + distance, data = lm_metrics_wide))
+summary(aov(intensity_max ~ site + product + distance, data = lm_metrics_wide))
+summary(aov(intensity_cumulative ~ site + product + distance, data = lm_metrics_wide))
