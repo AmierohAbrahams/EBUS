40140247e61568503e7bdd91c5bebcc46e155aca
AmierohAbrahams <3559244@myuwc.ac.za>
2020-05-21 16:21:01 +0200

    fixed extraction

--- Data_extraction/netCDF2csv.R
+++ Data_extraction/netCDF2csv.R
@@ -1,12 +1,16 @@
 # netCDF2csv
-# Loading libraries
-  
+# The purpose of this script is to...
+# The steps taken are:
+# 1: Setup environment
+# 2: Extract the OISST data
+# 3: Extract the ERA5 data
+
+# 1: Setup environment -------------------------------------------------------------------------------------
 library(ncdf4) # library for processing netCDFs
 library(ncdf4)
 library(data.table)
 library(tidyverse)
 library(reshape2)
-# library(plyr)
 library(lubridate)
 library(stringr)
 library(doMC); doMC::registerDoMC(cores = 4)
@@ -22,13 +26,16 @@ library(heatwaveR)
 # Marisol García-Reyes 1 *, William J. Sydeman 1 , David S. Schoeman 2 ,
 # Ryan R. Rykaczewski 3 , Bryan A. Black 4 , Albertus J. Smit 5 and Steven J. Bograd 6
 
+
+# 2: Extract the OISST data ---------------------------------------------------------------------------------
+
 bbox <- data.frame(BC = c(-35, -25, 15, 20), # Benguela Current
                    CC = c(15, 45, 340, 355), # Canary Current
                    CalC = c(25, 45, 225, 240), # California Current
                    HC = c(-45.5, -7.5, 275, 290), # Humboldt Current
                    row.names = c("latmin", "latmax", "lonmin", "lonmax"))
 
-# Setting path
+
 nc.dir <- "/home/amieroh/Documents/Data/Datasets/AVHRR/OISST"
 csv.dir <- "/home/amieroh/Documents/Data/Datasets/AVHRR/spatial" # Also stored on my harddrive
 
@@ -75,37 +82,17 @@ CC_temp <- CC-avhrr-only-v2.Document-Document.csv
 CalC_temp <- CalC-avhrr-only-v2.Document-Document.csv
 HC_temp <- HC-avhrr-only-v2.Document-Document.csv
 
-HC_temp <- read_csv("~/Documents/Data/Datasets/AVHRR/spatial/HC-avhrr-only-v2.Document-Document.csv", col_names = c("lon", "lat", "temp", "date"))
-save(HC_temp , file = "HC_temp.RData")
 
-################  ERA 5    ##################3
+# 2: Extract the ERA5 data ---------------------------------------------------------------------------------
 # https://cds.climate.copernicus.eu/api-how-to#use-the-cds-api-client-for-data-access
-
 # Extracting ERA 5 wind u and v variables
 # N/W/S/E coordinate format when downloading
 # -26.00/15.00/-36.00/22.00
 
-# Loading libraries
-library(stringr)
-library(tidyverse)
-library(reshape2)
-library(ncdf4) # library for processing netCDFs
-# library(plyr)
-library(lubridate)
-library(data.table)
-library(doMC); doMC::registerDoMC(cores = 7)
+# This extraction is for the Benguela Current the same is repeated for the Canary, California and Humboldt current
 
-# ERA.dir <- "/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate"
-# ERA.csv.dir <- "/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/Extracted"
-
-####### u-wind 2008-2019
-# #          1         2         3         4
-# # 12345678901234567890123456789012345678901
-# # wu_daily08-19.nc
-
-
-
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/BC/wind_daily81-99_165.nc'
+# EXtracting the u variable - 
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/BC/wind_165.nc'
 nc <- nc_open(ncFile)
 fNameStem <- substr(basename(ncFile), 1, 13)
 u_10 <- ncvar_get(nc, varid = "u10") %>%
@@ -124,13 +111,12 @@ t2 <- tidy %>%
 new <- cbind(t2, tidy)
 new <- new %>% 
   select(lon,lat,year,month ,day,u_10)
-u_10_df_BC <- new %>%
+BC_u <- new %>%
   unite(year, month, day, col = "date", sep = "-")
+# save(BC_u , file = "data/BC_u.RData")
 
-# save(u_10_df_BC , file = "data/u_10_df_BC.RData")
-
-
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/BC/wind_165.nc'
+# EXtracting the v variable - 
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/BC/wind_166.nc'
 nc <- nc_open(ncFile)
 fNameStem <- substr(basename(ncFile), 1, 14)
 u_10.2 <- ncvar_get(nc, varid = "u10") %>%
@@ -149,125 +135,13 @@ t2 <- tidy %>%
 new <- cbind(t2, tidy)
 new <- new %>% 
   select(lon,lat,year,month ,day,u_10)
-u_10.2df_BC <- new %>%
+BC_v <- new %>%
   unite(year, month, day, col = "date", sep = "-")
+# save(BC_v , file = "data/BC_v.RData")
 
-# BC_wind <- rbind(u_10_df_BC,u_10.2df_BC)
+# BC_fin <- rbind(BC_u,BC_v)
+# save(BC_fin , file = "data/BC_fin.RData")
+# save(HC_fin , file = "data/HC_fin.RData")
+# save(CC_fin , file = "data/CC_fin.RData")
+# save(CalC_fin , file = "data/CalC_fin.RData")
 
-load("data/BC_wind.RData")
-BC_wind <-rbind(BC_wind,u_10.2df_BC)
-
-# save(BC_wind , file = "data/BC_wind.RData")
-# THis file too big- Get from 2008-2012
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wu_daily08-19.nc'
-nc <- nc_open(ncFile)
-fNameStem <- substr(basename(ncFile), 1, 14)
-u_10.3_df <- ncvar_get(nc, varid = "u10") %>%
-  round(4)
-dimnames(u_10.3_df) <- list(lon = nc$dim$lon$vals,
-                            lat = nc$dim$lat$vals,
-                            time = nc$dim$time$vals)
-nc_close(nc)
-u_10.3_df <- as_tibble(melt(u_10.3_df, value.name = "u_10"))
-u_10.3_df$time <- as.POSIXct(u_10.3_df$time * 60 * 60, origin = "1900-01-01")
-tidy <- u_10.3_df %>%
-  separate(col = time, into = c("year", "month","day1"), sep = "-")
-t2 <- tidy %>%
-  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
-  select(day)
-new <- cbind(t2, tidy)
-new <- new %>% 
-  select(lon,lat,year,month ,day,u_10)
-u_10.2df <- new %>%
-  unite(year, month, day, col = "date", sep = "-")
-
-
-
-# save(u_10.2df_BC , file = "data/u_10.2df_BC.RData")
-# BC_wind <- rbind(u_10_df_BC,u_10.2df_BC)
-# save(BC_wind , file = "data/BC_wind.RData")
-# 165 - u 
-# 166 - v
-################################################################################################################################
-
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wv_daily08-19.nc'
-nc <- nc_open(ncFile)
-fNameStem <- substr(basename(ncFile), 1, 14)
-v_10.3_df <- ncvar_get(nc, varid = "v10") %>%
-  round(4)
-dimnames(v_10.3_df) <- list(lon = nc$dim$lon$vals,
-                            lat = nc$dim$lat$vals,
-                            time = nc$dim$time$vals)
-nc_close(nc)
-v_10.3_df <- as_tibble(melt(v_10.3_df, value.name = "u_10"))
-v_10.3_df$time <- as.POSIXct(v_10.3_df$time * 60 * 60, origin = "1900-01-01")
-tidy <- v_10.3_df %>%
-  separate(col = time, into = c("year", "month","day1"), sep = "-")
-t2 <- tidy %>%
-  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
-  select(day)
-new <- cbind(t2, tidy)
-new <- new %>% 
-  select(lon,lat,year,month ,day,u_10)
-u_10.2df <- new %>%
-  unite(year, month, day, col = "date", sep = "-")
-
-ERA_5_wind <- rbind(u_10_df, u_10.2_df, u_10.3_df)
-####################################################################################################################################################
-####################################################################################################################################################
-# 166 - V
-
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily81-99_166.nc'
-nc <- nc_open(ncFile)
-fNameStem <- substr(basename(ncFile), 1, 13)
-v_10 <- ncvar_get(nc, varid = "v10") %>%
-  round(4)
-dimnames(v_10) <- list(lon = nc$dim$lon$vals,
-                       lat = nc$dim$lat$vals,
-                       time = nc$dim$time$vals)
-nc_close(nc)
-v_10_df <- as_tibble(melt(v_10, value.name = "v_10"))
-v_10_df$time <- as.POSIXct(v_10_df$time * 60 * 60, origin = "1900-01-01")
-tidy <- v_10_df %>%
-  separate(col = time, into = c("year", "month","day1"), sep = "-")
-t2 <- tidy %>%
-  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
-  select(day)
-new <- cbind(t2, tidy)
-new <- new %>% 
-  select(lon,lat,year,month ,day,v_10)
-v_10_df_BC <- new %>%
-  unite(year, month, day, col = "date", sep = "-")
-
-# save(v_10_df_BC , file = "data/v_10_df_BC.RData")
-
-
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/BC/wind_166.nc'
-nc <- nc_open(ncFile)
-fNameStem <- substr(basename(ncFile), 1, 14)
-v_10.2 <- ncvar_get(nc, varid = "v10") %>%
-  round(4)
-dimnames(v_10.2) <- list(lon = nc$dim$lon$vals,
-                         lat = nc$dim$lat$vals,
-                         time = nc$dim$time$vals)
-nc_close(nc)
-v_10.2_df <- as_tibble(melt(v_10.2, value.name = "v_10"))
-v_10.2_df$time <- as.POSIXct(v_10.2_df$time * 60 * 60, origin = "1900-01-01")
-tidy <- v_10.2_df %>%
-  separate(col = time, into = c("year", "month","day1"), sep = "-")
-t2 <- tidy %>%
-  separate(col = day1, into = c("day", "hour"), sep = " ") %>% 
-  select(day)
-new <- cbind(t2, tidy)
-new <- new %>% 
-  select(lon,lat,year,month ,day,v_10)
-v_10.2df_BC <- new %>%
-  unite(year, month, day, col = "date", sep = "-")
-
-
-# save(v_10.2df_BC , file = "data/v_10.2df_BC.RData")
-# BC_vwind <- rbind(v_10_df_BC,v_10.2df_BC)
-# save(BC_vwind , file = "data/BC_vwind.RData")
-#BC_vwind <- rbind(BC_vwind,v_10.2df_BC)
-
-###################
