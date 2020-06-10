124f738c12c4f6a8358c1ed3a67dfeb7036c22a7
AmierohAbrahams <3559244@myuwc.ac.za>
2020-06-10 17:44:58 +0200

    Updated

--- Data_extraction/netCDF2csv.R
+++ Data_extraction/netCDF2csv.R
@@ -75,10 +75,10 @@ system.time(llply(ncList, read_nc, location = "CC", csv.dir = csv.dir, .parallel
 system.time(llply(ncList, read_nc, location = "CalC", csv.dir = csv.dir, .parallel = TRUE))
 system.time(llply(ncList, read_nc, location = "HC", csv.dir = csv.dir, .parallel = TRUE))
 
-BC_temp <- BC-avhrr-only-v2.Document-Document.csv
-CC_temp <- CC-avhrr-only-v2.Document-Document.csv
-CalC_temp <- CalC-avhrr-only-v2.Document-Document.csv
-HC_temp <- HC-avhrr-only-v2.Document-Document.csv
+BC <- BC-avhrr-only-v2.Document-Document.csv
+CC <- CC-avhrr-only-v2.Document-Document.csv
+CalC <- CalC-avhrr-only-v2.Document-Document.csv
+HC <- HC-avhrr-only-v2.Document-Document.csv
 
 
 # 2: Extract the ERA5 data ---------------------------------------------------------------------------------
@@ -86,12 +86,9 @@ HC_temp <- HC-avhrr-only-v2.Document-Document.csv
 # Extracting ERA 5 wind u and v variables
 # N/W/S/E coordinate format when downloading
 # -26.00/15.00/-36.00/22.00
-
 # This extraction is for the Benguela Current the same is repeated for the Canary, California and Humboldt current
-
 # New downloading ERA5 u and v variables
 # Extracting the data
-
 # To obtain ERA 5 wind data one needs to download u and v wind variables
 # These variables are collected every hour for every day
 # Here I extract u and v wind variables and then convert them to daily data in order to match the OISST daily temperature data
@@ -107,62 +104,6 @@ library(stringr)
 library(circular)
 library(doParallel); doParallel::registerDoParallel(cores = 8) 
 # RWS: I find doParallel to be more stable than doMC after recent updates to the tidyverse
-
-
-# Check netCDF info -------------------------------------------------------
-
-ncdump::NetCDF('~/R/forOthers/Amieroh/data.nc')
-
-
-# Load with ncdf4 ---------------------------------------------------------
-
-# Works but the time variables is not accurate
-# ncFile <- '/home/amieroh/Downloads/data.nc'
-ncFile <- '/home/amieroh/Downloads/data1.nc'
-nc <- nc_open(ncFile)
-u <- ncvar_get(nc, varid = "u10") %>%
-  round(4)
-dimnames(u) <- list(lon = nc$dim$lon$vals,
-                    lat = nc$dim$lat$vals,
-                    time = nc$dim$time$vals)
-# nc_close(nc)
-u <- as_tibble(melt(u, value.name = "u_10"))
-u$time <- as.Date(as.POSIXct(u$time * 60 * 60, origin = "1900-01-01"))
-BC_u <- u %>%
-  dplyr::rename(t = time) %>% 
-  group_by(lon, lat, t) %>% 
-  summarise(u_10 = mean(u_10, na.rm = T))
-
-# Test coords
-u_lon <- nc$dim$lon$vals
-u_lat <- nc$dim$lat$vals
-nc_close(nc)
-
-# Test time creation
-u_test <- u %>% 
-  filter(lon == u$lon[1], lat == u$lat[1]) %>% 
-  mutate(time = as.Date(as.POSIXct(time * 60 * 60, origin = "1900-01-01")))
-
-
-
-  # group_by(lon, lat, t) %>%
-  # summarise(u10 = mean(u10, na.rm = T), # To get daily values from hourly
-  #           v10 = mean(v10,na.rm = T)) 
-
-# This is the latest data up to 2020 saved as BC/CC/CalC/HC in the complete_data folder
-# Latest OISST data for rthe corrrect region is on Hardrive and in data/OISST file from 2018-2020
-
-CC_temp <- read_csv("~/Documents/CC-avhrr-only-v2.Document-Document.csv", col_names = c("lon", "lat", "temp", "date"))
-CalC_temp <- read_csv("~/Documents/CalC-avhrr-only-v2.Document-Document.csv", col_names = c("lon", "lat", "temp", "date"))
-HC_temp <- read_csv("~/Documents/HC-avhrr-only-v2.Document-Document.csv", col_names = c("lon", "lat", "temp", "date"))
-BC_temp <- read_csv("~/Documents/BC-avhrr-only-v2.Document-Document.csv", col_names = c("lon", "lat", "temp", "date"))
-OISST_HC <- OISST_HC %>% 
-  rename(date = t)
-HC <- rbind(HC_temp,OISST_HC)
-save(HC, file = "data_complete/HC.RData") 
-rm(HC); gc()
-rm(HC_temp); gc()
-rm(OISST_HC);gc()
 # The AJ way ---------------------------------------------------------------------------------------------------------------------------------
 nc <- nc_open("/home/amieroh/Downloads/data1.nc")
 u10 <- ncvar_get(nc, varid = "u10")
@@ -194,12 +135,15 @@ wind1982 <- cbind(u10_df,v10_df)4
 
 ###################################################################################################
 # Load with tidync --------------------------------------------------------
+# Load with tidync --------------------------------------------------------
 rm(BC_match);gc()
 rm(BC_match1982);gc()
 rm(BC_wind_fin);gc()
 rm(wind_daily);gc()
 
-wind_daily <- tidync("/home/amieroh/Downloads/BC_wind/data39.nc") %>%
+# rm(wind_daily_2)
+
+wind_daily <- tidync("/home/amieroh/Downloads/BC_wind/data4.nc") %>%
   hyper_tibble() %>% 
   dplyr::select(longitude, latitude, time, v10, u10) %>% 
   rename(lon = longitude,
@@ -207,7 +151,19 @@ wind_daily <- tidync("/home/amieroh/Downloads/BC_wind/data39.nc") %>%
   mutate(t = as.Date(as.POSIXct(time * 60 * 60, origin = "1900-01-01"))) %>% 
   dplyr::select(-time)
 
-BC_wind_fin <- wind_daily %>% 
+# wind_daily_2 <- tidync("/home/amieroh/Downloads/BC_wind/data2.nc") %>%
+#   hyper_tibble() %>%
+#   dplyr::select(longitude, latitude, time, v10, u10) %>%
+#   rename(lon = longitude,
+#          lat = latitude) %>%
+#   mutate(t = as.Date(as.POSIXct(time * 60 * 60, origin = "1900-01-01"))) %>%
+#  dplyr::select(-time)
+# 
+# combined <- rbind(wind_daily,wind_daily_2)
+
+combined <- rbind(combined,wind_daily)
+
+BC_wind_fin <- combined %>% 
   mutate( lat = lat - 0.125,
           lon = lon + 0.125) %>% 
   rename(date = t)
@@ -224,4 +180,5 @@ BC_match1982 <- match_func(temp_df = BC_long, wind_df = BC_wind_fin)
 
 combined <- rbind(combined,BC_match1982)
 save(combined, file = "data_complete/combined.RData")
+
 
